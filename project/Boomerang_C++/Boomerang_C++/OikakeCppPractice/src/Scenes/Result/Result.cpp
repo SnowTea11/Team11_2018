ce3e6bcda2dc5ed5b62c650d4945bbf6532cc5e6
hyuge63 <hyuge63@gmail.com>
2019-02-17 21:04:33 +0900

    一番新しい

--- project/Boomerang_C++/Boomerang_C++/OikakeCppPractice/src/Scenes/Result/Result.cpp
+++ project/Boomerang_C++/Boomerang_C++/OikakeCppPractice/src/Scenes/Result/Result.cpp
@@ -11,6 +11,7 @@ Result::Result(WorldPtr & world)
 	, world(world)
 	, renderer(Renderer::GetInstance())
 {
+	CursorPosition = 120;
 }
 
 Result::~Result()
@@ -21,20 +22,28 @@ void Result::LoadAssets()
 {
 	renderer.LoadTexture(Assets::Texture::Result, "result.png");
 	renderer.LoadTexture(Assets::Texture::Number, "number.png");
-
+	renderer.LoadTexture(Assets::Texture::Cursor, "cursur.png");
 }
 
 void Result::Initialize()
 {
-	CursorPosition = 120;
-
 	isEnd = false;
 	world->Initialize();
 	world->AddActor_Back(ActorGroup::UI, std::make_shared<Score>(world.get()));
+
+	//!ワールドのイベントメッセージを受け取る
+	world->AddEventMessageListener([&](EventMessage message, void* param)
+	{
+		HandleMessage(message, param);
+	});
 }
+
 
 void Result::Update(float deltaTime)
 {
+	Input::GetInstance().Update();
+	world->Update(deltaTime);
+
 	if (Input::GetInstance().GetKeyBoard().IsDown(KEY_INPUT_RIGHT))
 	{
 		CursorPosition = 650;
@@ -44,7 +53,7 @@ void Result::Update(float deltaTime)
 		CursorPosition = 120;
 	}
 
-	if (CursorPosition = 120)
+	if (CursorPosition == 120)
 	{
 		nextScene = Scene::Stage2;
 	}
@@ -53,18 +62,20 @@ void Result::Update(float deltaTime)
 		nextScene = Scene::StageSelect;
 	}
 
-	Input::GetInstance().Update();
+
 	if (Input::GetInstance().GetKeyBoard().IsDown(KEY_INPUT_SPACE))
 	{
 		isEnd = true;
 	}
-	world->Update(deltaTime);
+
 }
 
 void Result::Draw() const
 {
 	renderer.DrawTexture(Assets::Texture::Result);
+	renderer.DrawTexture(Assets::Texture::Cursor, Vector2(CursorPosition, 620));
 	world->Draw(renderer);
+
 }
 
 bool Result::IsEnd() const
@@ -85,6 +96,9 @@ void Result::Finalize()
 
 void Result::HandleMessage(EventMessage message, void * param)
 {
-	
+	if (message == EventMessage::GameEnd)
+	{
+		isEnd = true;
+	}
 }
 
